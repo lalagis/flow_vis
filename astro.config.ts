b66cd58c7a64e5473553fd89472cb362a294fd1c
cirno <cirnoqvq@163.com>
2023-09-30 20:46:16 +0800

    update: github pages deploy

--- astro.config.ts
+++ astro.config.ts
@@ -1,27 +1,29 @@
 import { defineConfig } from 'astro/config'
 import million from 'million/compiler'
 import UnoCSS from 'unocss/astro'
-import react from "@astrojs/react"
+import react from '@astrojs/react'
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://lalagis.github.io',
+  base: '/flow_vis',
+
   vite: {
-    plugins: [million.vite({
-      mode: 'react',
-      server: true,
-      auto: true
-    })],
-    
+    plugins: [
+      million.vite({
+        mode: 'react',
+        server: true,
+        auto: true,
+      }),
+    ],
+
     test: {
       globals: true,
       environment: 'jsdom',
       coverage: {
-        reporter: ['text', 'json', 'html']
-      }
-    }
+        reporter: ['text', 'json', 'html'],
+      },
+    },
   },
-  integrations: [
-    react(),
-    UnoCSS({ injectReset: true })
-  ]
-});
+  integrations: [react(), UnoCSS({ injectReset: true })],
+})
