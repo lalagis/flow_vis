3126687079f2abc76321682b3b9c8a51334407a2
cirno <cirnoqvq@163.com>
2023-09-30 19:01:25 +0800

    update: react & vitest

--- astro.config.ts
+++ astro.config.ts
@@ -1,16 +1,27 @@
 import { defineConfig } from 'astro/config'
 import million from 'million/compiler'
 import UnoCSS from 'unocss/astro'
+import react from "@astrojs/react"
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
-    plugins: [million.vite({ mode: 'react', server: true, auto: true })]
+    plugins: [million.vite({
+      mode: 'react',
+      server: true,
+      auto: true
+    })],
+    
+    test: {
+      globals: true,
+      environment: 'jsdom',
+      coverage: {
+        reporter: ['text', 'json', 'html']
+      }
+    }
   },
-
   integrations: [
-    UnoCSS({
-      injectReset: true
-    }),
-  ],
-})
+    react(),
+    UnoCSS({ injectReset: true })
+  ]
+});
