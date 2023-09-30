2235ca20242c7d18bd3cc5333dbab8bb1718b31a
cirno <cirnoqvq@163.com>
2023-09-30 18:44:35 +0800

    update: unocss

--- astro.config.ts
+++ astro.config.ts
@@ -1,9 +1,16 @@
 import { defineConfig } from 'astro/config'
 import million from 'million/compiler'
+import UnoCSS from 'unocss/astro'
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [million.vite({ mode: 'react', server: true, auto: true })]
-  }
+  },
+
+  integrations: [
+    UnoCSS({
+      injectReset: true
+    }),
+  ],
 })
